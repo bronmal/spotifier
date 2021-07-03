dfaa553b86beeea9b86063ea32766b851be08a9f
Bronmal <tri2004@yandex.ru>
2021-07-03 19:57:21 +0300

    залупа, мне лень

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 ID = 'b1fa88d88e3042d8b40e21af7e55a0ba'
 SECRET = '0b6e4567fb7b4cf0b60243579bccf94c'
-REDIRECT = 'http://192.168.1.102:8080/auth_spotify'
+REDIRECT = 'http://192.168.8.103:8080/auth_spotify'
