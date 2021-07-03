01849dede3a1866d90bd3ef469021d2ae4b79aa5
Bronmal <tri2004@yandex.ru>
2021-07-03 15:33:45 +0300

    удалил хлам

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 ID = 'b1fa88d88e3042d8b40e21af7e55a0ba'
 SECRET = '0b6e4567fb7b4cf0b60243579bccf94c'
-REDIRECT = 'http://192.168.1.64:8080/auth_spotify'
-
-# http://192.168.1.43:8080/replacing
+REDIRECT = 'http://192.168.1.102:8080/auth_spotify'
