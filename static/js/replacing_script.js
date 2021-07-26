a49a5e3febc687f5ad6d62e9cd5558d350b9e2d3
Jayko321 <ldzkeymylp@gmail.com>
2021-07-26 16:38:45 +0300

    Сделал для скриптов отдельную папку, поместил скрипт в отдельный файл, сделал скрипт который говорит что много треков

---
+++ static/js/replacing_script.js
@@ -0,0 +1,71 @@
+let success = false
+let intervalId;
+let intervalId2;
+
+async function a() {
+    let m = document.getElementById("transfer"),
+        s = m.innerHTML,
+        i = 0;
+    intervalId = setInterval(function () {
+        m.innerHTML = s + Array(i++ % 5).join('.');
+    }, 500);
+}
+
+async function d(){
+    let a = document.getElementById("hidden")
+    intervalId2 = setInterval(() => {
+        a.style.fontWeight = 'normal'
+        a.innerHTML = "Похоже у вас много треков вконтакте, не беспокойтесь мы все ещё переносим их"
+        clearInterval(intervalId2)
+    }, 30000)  
+    a.style.fontWeight = 'bold' 
+}
+
+async function b(){
+    $(function () {
+        $.ajax({
+            url: '/transfer',
+            type: 'GET',
+            success: function (response) {
+                document.getElementById("transfer").innerHTML = "завершено"
+                const json = jQuery.parseJSON(response);
+                if (isNaN(json.errors)) {
+                    for (let key in json.errors) {
+                        $("#errors_transfer").append("<option>" + json.errors[key] + "</option>")
+                    }
+                    $("#errors").css("display", "block");
+                }
+                clearInterval(intervalId)
+                clearInterval(intervalId2)
+                c()
+            },
+            error: function (error) {}
+        });
+    })
+}
+
+async function c() {
+    $(function () {
+        $.ajax({
+            url: '/pay',
+            type: 'GET',
+            success: function (response) {
+                const json = jQuery.parseJSON(response);
+                if (json.url_to_pay !== null)
+                {
+                    document.getElementById("transfer").setAttribute("onclick", 'window.location.href = ' + "'" + json.url_to_pay + "'")
+                    document.getElementById("transfer").innerHTML = "Оплатить 69 руб*"
+                    document.getElementById("user_agreement").style.display = "block"
+                    document.getElementById("hidden").innerHTML = "Вы перенесли 100 треков, чтобы переносить без ограничений заплатите 69 рублей"
+                    document.getElementsByClassName("btn-start")[0].style.marginTop = 10 + "px";
+                }
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    })
+}
+a()
+d()
+b()
