622538136e3c8f628d4ff00c180f1882f325daa2
Bronmal <tri2004@yandex.ru>
2021-08-12 15:25:59 +0300

    фикс дизайна

--- db.py
+++ db.py
@@ -2,11 +2,13 @@ import pymysql
 import json
 from logger import log
 
+
 @log
 def create_con():
     con = pymysql.connect(user='bronmal', host='bronmal.mysql.eu.pythonanywhere-services.com',
                           password='andrey5550100', database='bronmal$pay')
     return con
+
 
 @log
 def in_db(logins):
