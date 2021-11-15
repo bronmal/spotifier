4016d8b6be932a6a73feee86172cfd1bbcc4366b
Bronmal <tri2004@yandex.ru>
2021-11-15 14:39:37 +0300

    =ijnrvijnvirn

---
+++ config.py
@@ -0,0 +1,9 @@
+URL = 'http://127.0.0.1:5000'
+ID = 'b1fa88d88e3042d8b40e21af7e55a0ba'
+SECRET = '0b6e4567fb7b4cf0b60243579bccf94c'
+REDIRECT = f'{URL}/auth_spotify'
+MAX_TRACKS = 100
+GOOGLE_ID = '835837949353-evpleoubufejftjurlln3as591072457.apps.googleusercontent.com'
+GOOGLE_SECRET = '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'
+GOOGLE_AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent'
+GOOGLE_ACCESS_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
