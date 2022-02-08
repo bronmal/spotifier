5c197673966af4169737470df4d9f6433afc6e45
Bronmal <tri2004@yandex.ru>
2022-02-08 17:18:05 +0300

    Полное добавление возможности автоплатежа

---
+++ subscribtion_provider.py
@@ -0,0 +1,29 @@
+import db
+from datetime import datetime
+from yookassa import Configuration
+from yookassa import Payment
+from yookassa.domain.models.currency import Currency
+from yookassa.domain.request.payment_request_builder import PaymentRequestBuilder
+
+Configuration.configure(822381, 'test_Z0bQfQgOZa2d_KbHJmo4j65IfoiG7OPfdCLvfrz5VtE')
+
+users_info = db.get_info_all_users()
+date_now = str(datetime.now().day) + '.' + str(datetime.now().month)
+for i in users_info:
+    date_db = datetime.strptime(i['date_end'], '%d.%m')
+    date_db_format = str(date_db.day) + '.' + str(date_db.month)
+    if date_db_format == date_now:
+        builder = PaymentRequestBuilder()
+        builder.set_amount({"value": 149, "currency": Currency.RUB}) \
+            .set_capture(True) \
+            .set_payment_method_id(i['payment_id']) \
+            .set_description(f"Оплата подписки за {datetime.now()}")
+        request = builder.build()
+        res = Payment.create(request)
+        if Payment.find_one(res.id).paid:
+            db.user_payed(i['user_id'], res.payment_method.id)
+        else:
+            db.delete_sub(i['user_id'])
+
+
+
