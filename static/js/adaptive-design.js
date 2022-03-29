d1f9b5f1a6d7edeabb2a978cb16908eedbda5d17
jayko <ldzkeymylp@gmail.com>
2022-03-29 21:27:58 +0300

    попап и перенос музыки

--- static/js/adaptive-design.js
+++ static/js/adaptive-design.js
@@ -2,12 +2,20 @@ defaultWidth = 1440;
 defaultHeight = 900;
 deltaW = 1;
 deltaH = 1;
+root = document.querySelector(":root");
 
 function changeDelta() {
     updateDelta();
-    let r = document.querySelector(":root");
-    r.style.setProperty('--deltaH', deltaH);
-    r.style.setProperty('--deltaW', deltaW);
+    if (getResolution().Height > 600 && getResolution().Width > 1000) {
+        document.body.style.width = `none`
+        root.style.setProperty('--deltaH', deltaH);
+        root.style.setProperty('--deltaW', deltaW);
+    } else {
+        deltaH = 1;
+        deltaW = 1;
+        document.body.style.width = `calc(1440px*var(--deltaW))`
+    }
+
 }
 
 function updateDelta() {
@@ -18,8 +26,8 @@ function updateDelta() {
 
 function getResolution() {
     Resolution = {
-        "Width": Math.max(window.innerWidth),
-        "Height": Math.max(window.innerHeight)
+        "Width": $(window).width(),
+        "Height": $(window).height()
     }
 
     return Resolution;
