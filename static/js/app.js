ad13e13dcc323dec8f34d89f16270e7f5a168ef5
jayko <ldzkeymylp@gmail.com>
2022-04-07 18:24:44 +0300

    Уродливый крестик + синхронный запросы и более полное использование MutationObserver'a

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
-mutableElements = [document.querySelector('.app.chosen.option'), document.querySelector('.app.chosen.add'), document.querySelector('.app.chosen.transfer')]
-localizedVars = { tracks: _('треки'), playlists: _('плейлисты'), artists: _('артисты'), albums: _("альбомы") }
+const mutableElements = [document.querySelector('.app.chosen.option'), document.querySelector('.app.chosen.add'), document.querySelector('.app.chosen.transfer')]
+const localizedVars = { tracks: _('треки'), playlists: _('плейлисты'), artists: _('артисты'), albums: _("альбомы") }
 
 data = {
     "tracks": [],
@@ -19,15 +19,34 @@ function appOnLoad() {
 }
 
 async function bindEvents() {
+    const songList = document.querySelector('.app.main-container')
+    const config = { childList: true }
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                let count = document.querySelector(`.app.chosen.option`).innerHTML
+                let length = songList.childElementCount - 5;
+                document.querySelector(`.app.chosen.option`).innerHTML = count.substring(0, count.indexOf(':') + 2) + String(length)
+                let height = 208 + length * 50;
+
+                document.body.style.height = `calc(${height}px*var(--deltaH))`;
+                songList.style.height = `calc(${height}px*var(--deltaH))`;
+            }
+        }
+    })
+    observer.observe(songList, config);
+
+
+
     document.querySelector('.app.added-services.right-arrow-btn').addEventListener('click', () => { next('right') });
     document.querySelector('.app.added-services.left-arrow-btn').addEventListener('click', () => { next('left') });
     document.querySelector('.app.personal-info.logout').addEventListener('click', () => { logout() });
     document.querySelector('.app.menu-button').addEventListener('click', () => { openMenu() });
     document.querySelector('.app.transfer-music.non-selectable').addEventListener('click', () => { showTransferPopUp() });
     document.querySelector('.app.music-container.my-box.playlists').addEventListener('click', () => { displayData('playlists', localizedVars.playlists) });
-    document.querySelector('.app.music-container.my-box.tracks').addEventListener('click', () => { displayData('tracks', localizedVars.playlists) });
-    document.querySelector('.app.music-container.my-box.artists').addEventListener('click', () => { displayData('artists', localizedVars.playlists) });
-    document.querySelector('.app.music-container.my-box.albums').addEventListener('click', () => { displayData('albums', localizedVars.playlists) });
+    document.querySelector('.app.music-container.my-box.tracks').addEventListener('click', () => { displayData('tracks', localizedVars.tracks) });
+    document.querySelector('.app.music-container.my-box.artists').addEventListener('click', () => { displayData('artists', localizedVars.artists) });
+    document.querySelector('.app.music-container.my-box.albums').addEventListener('click', () => { displayData('albums', localizedVars.albums) });
     document.querySelector('.app.chosen.transfer.non-selectable').addEventListener('click', () => { showTransferPopUp() });
     document.querySelector('.app.song.top-part.checkbox').addEventListener('click', () => { chooseAllSongs() });
     document.querySelector('.app.song.top-part.delete').addEventListener('click', () => { deleteAllSongs() });
@@ -87,18 +106,23 @@ async function parseServiceData(url, service, offset) {
     })
 }
 
-function sendData(to_service) {
-    data.to_service = to_service;
-    $(() => {
-        $.ajax({
-            url: 'http://127.0.0.1:5000/send_audio',
-            type: 'POST',
-            contentType: 'application/json;charset=UTF-8',
-            dataType: 'json',
-            data: JSON.stringify(data),
-            success: (response) => {
-                console.log(response);
-            }
+async function sendData(to_service) {
+    return new Promise((resolve, reject) => {
+        data.to_service = to_service;
+        $(() => {
+            $.ajax({
+                url: '/send_audio',
+                type: 'POST',
+                contentType: 'application/json;charset=UTF-8',
+                dataType: 'json',
+                data: JSON.stringify(data),
+                success: (response) => {
+                    resolve(response)
+                },
+                error: (response) => {
+                    reject(response)
+                }
+            })
         })
     })
 }
@@ -108,88 +132,59 @@ async function displayData(data, type) {
         deleteAllSongs();
         replaceAllChosen(type);
     }
-    length = 0
+    if (currentOption === type) return;
     currentOption = type
-
-    parseData('http://127.0.0.1:5000/get_services').then((response) => {
-        let services = JSON.parse(response)
-        for (i = 0; i < services.length; i++) {
-            if (services[i] !== 'yandex' && services[i] !== 'deezer') {
-                recursiveAddSongs(services[i], 0, data)
-
-            } else {
-                addSongs(services[i], data);
-            }
-        }
-    })
-
-
+    setCount(0, "option");
+    let services = JSON.parse(await parseData('/get_services'))
+    for (let service of services.sort()) {
+        await recursiveGetSongs(service, 0, data, type)
+    }
 }
 
-async function addSongs(service, data) {
-    parseServiceData('http://127.0.0.1:5000/get_audio', service, 0).then((response) => {
-        let mainContainer = document.querySelector('.app.main-container')
-        let p_data = response[data]
-        length += p_data.length
+async function recursiveGetSongs(service, offset, data, type) {
+    let mainContainer = document.querySelector('.app.main-container')
 
-        for (i in p_data) {
-            add(p_data[i].id, p_data[i].title, p_data[i].service, p_data[i].album == undefined ? '' : p_data[i].album, p_data[i].artist == undefined ? '' : p_data[i].artist, mainContainer, data)
+    let response = await parseServiceData('/get_audio', service, offset)
+    if (currentOption !== type) {
+        return null;
+    }
+    let ofs = offset + 15;
+    let p_data = response[data]
+    for (let song of p_data) {
+        add(song.id, song.title, song.service, song.album == undefined ? '' : song.album, song.artist == undefined ? '' : song.artist, mainContainer, data)
+    }
+    let len = p_data.length
+    if (len === 15) {
+        let songs = await recursiveGetSongs(service, ofs, data, type);
+        for (let song of songs) {
+            p_data.push(song)
         }
-        setCount(length, "option")
-
-        let height = 162 + (length + 1) * (parseInt(window.getComputedStyle(document.querySelector('.app.song')).height.substring(0, window.getComputedStyle(document.querySelector('.app.song')).height.length - 2)) + 3);
-
-        document.body.style.height = `calc(${height}px*var(--deltaH))`;
-        mainContainer.style.height = `calc(${height}px*var(--deltaH))`;
-    })
-    return;
+    }
+    console.log(p_data);
+    return p_data;
 }
 
-async function recursiveAddSongs(service, offset, data) {
-    parseServiceData('http://127.0.0.1:5000/get_audio', service, offset).then((response) => {
-        offset += 15
-        let mainContainer = document.querySelector('.app.main-container')
-        let p_data = response[data]
-        let len = p_data.length
-        if (len === 15) {
-            recursiveAddSongs(service, offset, data);
-        }
-        length += p_data.length
-
-        for (i in p_data) {
-            add(p_data[i].id + offset, p_data[i].title, p_data[i].service, p_data[i].album == undefined ? '' : p_data[i].album, p_data[i].artist == undefined ? '' : p_data[i].artist, mainContainer, data)
-        }
-        setCount(length, "option")
-
-        let height = 162 + (length + 1) * (parseInt(window.getComputedStyle(document.querySelector('.app.song')).height.substring(0, window.getComputedStyle(document.querySelector('.app.song')).height.length - 2)) + 3);
-
-        document.body.style.height = `calc(${height}px*var(--deltaH))`;
-        mainContainer.style.height = `calc(${height}px*var(--deltaH))`;
-    })
-}
-
-function setCount(value, element) {
+async function setCount(value, element) {
     if (value >= 0) {
         let count = document.querySelector(`.app.chosen.${element}`).innerHTML
         document.querySelector(`.app.chosen.${element}`).innerHTML = count.substring(0, count.indexOf(':') + 2) + String(value)
     }
-
 }
 
-function getCount(element) {
+async function getCount(element) {
     let count = document.querySelector(`.app.chosen.${element}`).innerHTML
     let _ = parseInt(count.substring(count.indexOf(':') + 1));
     return _;
 }
 
-function replaceAllChosen(element) {
+async function replaceAllChosen(element) {
     chosenElement = element
     for (let i = 0; i < mutableElements.length; i++) {
-        mutableElements[i].innerHTML = replaceChosen(mutableElements[i].innerHTML, chosenElement)
+        replaceChosen(mutableElements[i].innerHTML, chosenElement).then((response) => { mutableElements[i].innerHTML = response })
     }
 }
 
-function replaceChosen(string, replaceString) {
+async function replaceChosen(string, replaceString) {
     let str = string.replace(currentOption, replaceString)
     return str
 }
@@ -249,7 +244,7 @@ async function openMenu() {
     }
 }
 
-async function add(id, title, service, album, artist, mainContainer, type) {
+function add(id, title, service, album, artist, mainContainer, type) {
     let fragment = new DocumentFragment();
     let song = document.createElement('div')
     let servicePath = ""
@@ -269,7 +264,7 @@ async function add(id, title, service, album, artist, mainContainer, type) {
     }
     song.className = `app song id${id}`
     song.innerHTML = `
-        <input class="app song checkbox" onclick="choose(document.querySelector('.app.song.id${id}'), '${type}')" type="checkbox"></input>
+        <input class="app song checkbox" type="checkbox"></input>
         <label class="app song label">${title}</label>
         <img src="${servicePath}" class="app song service ${service}"></img>
         <div class="app song option1">${artist}</div>
@@ -277,51 +272,51 @@ async function add(id, title, service, album, artist, mainContainer, type) {
         <img src="/static/images/change-btn.svg" class="app song change-btn"></img>
         <img src="/static/images/delete-btn.svg" class="app song delete-btn"></img>
         </input>`
-
     fragment.appendChild(song)
+    fragment.querySelector('.app.song.checkbox').addEventListener('click', () => { choose(document.querySelector(`.app.song.id${id}`), type) })
     fragment.querySelector('.app.song.delete-btn').addEventListener('click', () => { deleteSong(document.querySelector('.app.song.delete-btn')) })
     mainContainer.appendChild(fragment)
 }
 
-function choose(object, type) {
+async function choose(object, type) {
     let id = object.className.substring(object.className.indexOf("id") + 2)
     let service = object.childNodes[5].className.substring(object.childNodes[5].className.indexOf("service") + 8);
 
     let _ = { "id": id, "service": service }
 
-    if (data[`${type}`].some(item => (item.id === id && item.service === service))) {
+    if (data[type].some(item => (item.id === id && item.service === service))) {
         object.childNodes[1].checked = false;
         let index = 0;
-        for (key in data[`${type}`]) {
-            value = data[`${type}`][key];
+        for (key in data[type]) {
+            value = data[type][key];
             if (id === value.id && service === value.service) {
                 break;
             }
             index++;
         }
         data[`${type}`].splice(index, 1);
-        setCount(getCount("count") - 1, "count")
+        setCount(await getCount("count") - 1, "count")
         return;
     }
     object.childNodes[1].checked = true;
     data[`${type}`].push(_);
-    setCount(getCount("count") + 1, "count")
+    setCount(await getCount("count") + 1, "count")
 }
 
-function chooseAllSongs() {
+async function chooseAllSongs() {
     let tracks = document.querySelectorAll('.app.song')
     for (let i = 8; i < tracks.length; i += 8) {
-        choose(tracks[i], getKeyByValue(localizedVars, chosenElement))
+        await choose(tracks[i], getKeyByValue(localizedVars, chosenElement))
     }
 }
 
-function deleteSong(element) {
+async function deleteSong(element) {
     element.parentElement.parentElement.removeChild(element.parentElement)
-    setCount(getCount("option") - 1, "option")
-    setCount(getCount("count") - 1, "count")
+    setCount(await getCount("option") - 1, "option")
+    setCount(await getCount("count") - 1, "count")
 }
 
-function deleteAllSongs() {
+async function deleteAllSongs() {
     let mainContainer = document.querySelector('.app.main-container')
     let tracks = document.querySelectorAll('.app.song')
     for (let i = 9; i < tracks.length; i += 8) {
@@ -331,7 +326,7 @@ function deleteAllSongs() {
     mainContainer.style.height = "max-content";
 }
 
-function showTransferPopUp() {
+async function showTransferPopUp() {
     document.querySelector('.app.popup').style.display = 'flex'
     parseData('/get_services').then((response) => {
         const services = JSON.parse(response);
@@ -346,25 +341,26 @@ function showTransferPopUp() {
     })
 }
 
-function chooseService(service) {
+async function chooseService(service) {
     document.querySelector('.app.popup').style.display = 'none'
     sendData(service)
 }
 
 async function getServices() {
     parseData('/get_services').then((response) => {
-        let serviceElements = document.querySelectorAll('.app.added-services.service-container.service')
-        for (i = 0; i < serviceElements.length; i++) {
-            if (response.includes(serviceElements[i].className.substring(59, serviceElements[i].className.length))) {
-                serviceElements[i].classList.remove('not-connected')
-                serviceElements[i].removeAttribute('onclick')
-
+        const serviceElements = document.querySelectorAll('.app.added-services.service-container.service')
+        const services = JSON.parse(response)
+        for (const service of services) {
+            for (const serviceElement of serviceElements) {
+                if (serviceElement.classList.contains(service)) {
+                    serviceElement.classList.remove('not-connected')
+                }
             }
         }
     })
 }
 
-function next(direction) {
+async function next(direction) {
     if (direction === 'right') {
         let serviceBox = document.querySelector('.app.added-services.service-container.non-selectable');
         let firstService = serviceBox.firstElementChild;
